d08def50c391774df75df7c76366743e5b26984c
jbg32004 <149401415+jbg32004@users.noreply.github.com>
2023-12-01 14:32:10 -0500

    Update Lab11Prob02.java

--- Lab11Prob02.java
+++ Lab11Prob02.java
@@ -2,7 +2,12 @@
 public class Lab11Prob02 {
 
 	public static String reverseString(String s) {
-		s = String.format("%s%s%s", s.substring(s.length()-2, s.length()-1), reverseString(s), );
+		if(s.length() > 1) {
+			s = String.format("%s%s%s", 
+					s.substring(s.length()-2, s.length()-1), 
+					reverseString(s.substring(1, s.length()-1)), 
+					s.substring(0,1));
+		else
 			return s;
 	}
 	
