b8560e6b30a8238b1807b509b10dc499c38e92fa
jbg32004 <149401415+jbg32004@users.noreply.github.com>
2023-12-01 15:31:52 -0500

    Update Lab11Prob03.java

--- Lab11Prob03.java
+++ Lab11Prob03.java
@@ -1,4 +1,20 @@
 
 public class Lab11Prob03 {
 
+	public static String reverseString(String s) {
+		return reverseString(s, s.length()-1);
+	}
+	
+	public static String reverseString(String s, int i) {
+		if(i > 0)
+			return s.charAt(i) + reverseString(s, i-1);
+		else
+			return "" + s.charAt(0);
+	}
+
+
+	public static void main(String[] args) {
+		String s = "String";
+		System.out.println(reverseString(s));
+	}
 }
